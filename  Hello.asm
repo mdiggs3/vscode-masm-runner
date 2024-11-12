36607944952a5f96943b27f1a9d06dcd3ad1f761
mdiggs3 <mdiggs@algomau.ca>
2024-11-12 19:50:23 -0400

    Add Hello.asm file

---
+++  Hello.asm
@@ -0,0 +1,12 @@
+INCLUDE Irvine32.inc
+.data
+hello BYTE "Hello Assembly!",0
+
+.code
+main PROC
+        MOV EDX, OFFSET hello       ;Print the Hello Message
+        CALL WriteString
+        CALL CrLf
+        EXIT
+main ENDP
+END main
